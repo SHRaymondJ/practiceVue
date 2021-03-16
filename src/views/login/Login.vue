f0a7fc442edecc2441b110172d0b86641c176cc5
RaymondJ <396945399@qq.com>
2021-03-16 11:50:51 +0800

    1

---
+++ src/views/login/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="wrapper">
+    <img src="http://www.dell-lee.com/imgs/vue3/user.png" alt="" class="wrapper__img" />
+    <div class="wrapper__input">
+      <input type="text" class="wrapper__input__content" placeholder="请输入用户名" />
+    </div>
+    <div class="wrapper__input">
+      <input type="text" class="wrapper__input__content" placeholder="请输入密码" />
+    </div>
+    <div class="wrapper__login-button">登陆</div>
+    <div class="wrapper__login-link"><span>立即注册</span><span>忘记密码</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/variables.scss';
+.wrapper {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  &__img {
+    display: block;
+    margin: 0 auto 0.4rem auto;
+    width: 0.66rem;
+    height: 0.66rem;
+  }
+  &__input {
+    font-size: 0.16rem;
+    box-sizing: border-box;
+    height: 0.48rem;
+    margin: 0 0.4rem 0.16rem 0.4rem;
+    padding: 0 0.16rem;
+    background: #f9f9f9;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 6px;
+    &__content {
+      line-height: 0.48rem;
+      border: none;
+      outline: none;
+      width: 100%;
+      background: none;
+      color: $content-notice-fc;
+      &::placeholder {
+        color: $content-notice-fc;
+      }
+    }
+  }
+  &__login-button{
+    margin: 0.32rem .4rem .16rem .4rem;
+    line-height: .48rem;
+    background: #0091ff;
+    box-shadow: 0 .04rem .08rem 0 rgba(0,145,255,0.32);
+    border-radius: .04rem;
+    color: #fff;
+    font-size: .16rem;
+    text-align: center;
+  }
+  &__login-link{
+    font-size: .14rem;
+    text-align: center;
+    color: $content-notice-fc;
+    span{
+      padding: 0 .1rem;
+      border-right: 1px solid $content-notice-fc;
+      &:last-child{
+        border-right: none;
+      }
+    }
+  }
+}
+</style>
